b8abeb2535332f6856385b16d250e1e97f9f22c5
Christoph Schulthess <christoph@widerstand.digital>
2018-07-26 20:07:59 +0200

    first model iteration

--- MoMo/core/models.py
+++ MoMo/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class PaymentServiceProvider(models.Model):
+    fullname = models.CharField(max_length=100)
+    slug = models.SlugField(null=True, blank=True)
+
+class SaasInstance(models.Model):
+    fullname = models.CharField(max_length=100)
+    address = models.GenericIpAddressField(protocol='both')
+
+class Payment(models.Model):
+    reference_id = models.CharField(max_length=50)
+    amount = models.IntegerField(default=0)
+    date_received = models.DateTimeField()
+    account_id = models.ForeignKey(SaasInstance)
+    psp = models.ForeignKey(PaymentServiceProvider)
