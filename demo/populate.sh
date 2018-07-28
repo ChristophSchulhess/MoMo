fd6ce64511e8dc8733569d6522289ea6f1b4eafe
Christoph Schulthess <christoph@widerstand.digital>
2018-07-28 15:01:50 +0200

    populate script

--- demo/populate.sh
+++ demo/populate.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-set -x
+#set -x
 
-source bin/activate
+source ../bin/activate
 
 declare -rg CORE_URL="http://127.0.0.1:8000/"
 
 declare -rg PSP_PATH="payment_service_provider/"
 declare -rg SAAS_INSTANCE_PATH="saas_instance/"
 
-http --json "${CORE_URL}${PSP_PATH}" fullname="Iron Bank of Bravos"
-http --json "${CORE_URL}${PSP_PATH}" fullname="Gringots"
+echo "Populating MoMo models: PaymentServiceProvider, SaasInstance."
+
+http --json "${CORE_URL}${PSP_PATH}" fullname="Iron Bank of Bravos" > /dev/null
+http --json "${CORE_URL}${PSP_PATH}" fullname="Gringots" > /dev/null
 
 http --json "${CORE_URL}${SAAS_INSTANCE_PATH}" \
     fullname="Thomas Edison" \
-    url="http://127.0.0.1:8042"
+    url="http://127.0.0.1:8042" > /dev/null
 http --json "${CORE_URL}${SAAS_INSTANCE_PATH}" \
     fullname="Nicola Tesla" \
-    url="http://127.0.0.1:8043"
+    url="http://127.0.0.1:8043" > /dev/null
+
+echo $(http --pretty all "${CORE_URL}${PSP_PATH}")
+echo $(http --pretty all "${CORE_URL}${SAAS_INSTANCE_PATH}")
