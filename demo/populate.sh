4c7ba4d93a577c383a37c5e991f1d41be3acf142
Christoph Schulthess <christoph@widerstand.digital>
2018-07-28 13:50:14 +0200

    populate script revisited

--- demo/populate.sh
+++ demo/populate.sh
@@ -6,10 +6,15 @@ source bin/activate
 
 declare -rg CORE_URL="http://127.0.0.1:8000/"
 
-declare -rg PAYMENT_PATH="payment/"
 declare -rg PSP_PATH="payment_service_provider/"
-declare -rg PSP_ADAPTER_PATH="psp_adapter/"
 declare -rg SAAS_INSTANCE_PATH="saas_instance/"
 
-http --json "${CORE_URL}${PSP_PATH}" fullname="Bank of Africa"
+http --json "${CORE_URL}${PSP_PATH}" fullname="Iron Bank of Bravos"
 http --json "${CORE_URL}${PSP_PATH}" fullname="Gringots"
+
+http --json "${CORE_URL}${SAAS_INSTANCE_PATH}" \
+    fullname="Thomas Edison" \
+    url="http://127.0.0.1:8042"
+http --json "${CORE_URL}${SAAS_INSTANCE_PATH}" \
+    fullname="Nicola Tesla" \
+    url="http://127.0.0.1:8043"
