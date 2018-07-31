4e560b608bd999c13f4eefaf9cd02b373190fd25
Christoph Schulthess <christoph@widerstand.digital>
2018-07-31 11:00:32 +0200

    updated routers

--- MoMo/core/routers.py
+++ MoMo/core/routers.py
@@ -7,45 +7,80 @@ from core import models
 
 class Router():
     '''
-    Base class for MoMo routers.
+    Base class for MoMo routers. A return value (error state) of None refers to
+    a succesful transaction.
     '''
     def __init__(self):
-        pass
+        self.dest = None
 
     def route(self, data):
-        pass
+        '''
+        Basic route method to be overwritten by child routers. Simply attempts
+        to send the data and returns exceptions if a) a ConnectionError occurs
+        (i.e. SaaS instance is down) or b) the HTTP responses status code is an
+        error (i.e. 4XX or 5XX). Handling of these cases should be done in
+        views.
+        '''
+        try:
+            response = requests.post(self.dest.url, data)
+        except ConnectionError as connection_error:
+            return connection_error
+        return response.raise_for_status()
+
 
 class AccountIdRouter(Router):
     '''
     The AccountIdRouter will retrieve the url of the SaaS instance that
     corresponds to the account_id field of a given payment and send the payment
-    data there.
+    data there. DoesNotExist errors will be returned to the caller.
     '''
     def route(self, data):
-        dest = models.SaasInstance.objects.get(id=data['account_id'])
-        return requests.post(dest.url, data).raise_for_status()
+        try:
+            self.dest = models.SaasInstance.objects.get(id=data['account_id'])
+        except (models.SaasInstance.DoesNotExist) as does_not_exist:
+            return does_not_exist
+        return super().route(data)
+
 
 class ReferenceIdRouter(Router):
     '''
     The ReferenceIdRouter has to retrieve customer identifiers from the known
     SaaS instances since this information will likely be located in their
     domain. As soon as an instance sends a confirmation, it will dispatch the
-    payment data to that instance (We assume that instances provide some kind of
-    API to retrieve the required information).
+    payment data to that instance.
+
+    NOTE: It is assumed that instances provide some kind of API to retrieve the
+    required information. As there is no information about formats and calls,
+    the router will focus on status codes exclusively.
     '''
+    class ReferenceIdError(Exception):
+        '''
+        Custom error in case of unsuccessful localisation attempt.
+        '''
+        def __init__(self, reference_id):
+            super().__init__()
+
     def route(self, data):
+        # ReferenceIdError is returned to views if it occurs.
+        try:
+            self.dest = self.locate_reference_id(data['reference_id'])
+        except self.ReferenceIdError as reference_id_error:
+            return reference_id_error
+        return super().route(data)
+
+    def locate_reference_id(self, reference_id):
+        # Retrieve all SaaS instances from model
         saas_instances = models.SaasInstance.objects.all()
-        dest = None
+        # Try to determine which instance holds a customer with the respective
+        # reference_id.
         for instance in saas_instances:
-            # This call depends on the instance API
-            response = requests.get('{}/{}/'.format(instance.url,
-                data['reference_id']))
-            # Some way of verifying confirmation, depends on API as well
-            if response.status_code == 200 and len(response.json()) == 1:
-                dest = instance
-                break
-        if not dest:
-            return False
-        return requests.post(dest.url, data).raise_for_status()
-
-
+            try:
+                # This call depends on the instance API.
+                response = requests.get(instance.url)
+            except ConnectionError:
+                continue
+            # Some way of verifying confirmation, depends on API as well.
+            if response.status_code == 200:
+                return instance
+        # reference_id could not be located. Raise custom error.
+        raise self.ReferenceIdError(reference_id)
